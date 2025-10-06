e2e2c893894a21a33de61e62ab78e61ce5bd5202
Alex Dickson <alex.dickson@immfly.com>
2025-10-06 19:34:40 +0200

    Header kind of done

--- apps/site/app/layouts/header.tsx
+++ apps/site/app/layouts/header.tsx
@@ -20,8 +20,41 @@ const MobileMenu = ({
         open ? 'translate-x-0' : '-translate-x-full'
       )}
     >
-      <X onClick={() => setOpen(false)} className='text-gray-400' />
-      Hola
+      <div className='flex px-4 pb-2 pt-5'>
+        <button
+          type='button'
+          onClick={() => setOpen(false)}
+          className='relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400'
+        >
+          <span className='absolute -inset-0.5' />
+          <span className='sr-only'>Close menu</span>
+          <X className='text-gray-400' />
+        </button>
+      </div>
+      <div className='space-y-6 px-4 py-6'>
+        <div className='flow-root'>
+          <a href='#' className='-m-2 block p-2 font-medium text-gray-900'>
+            Sign in
+          </a>
+        </div>
+        <div className='flow-root'>
+          <a href='#' className='-m-2 block p-2 font-medium text-gray-900'>
+            Create account
+          </a>
+        </div>
+      </div>
+
+      <div className='border-t border-gray-200 px-4 py-6'>
+        <a href='#' className='-m-2 flex items-center p-2'>
+          <img
+            alt=''
+            src='https://tailwindcss.com/plus-assets/img/flags/flag-canada.svg'
+            className='block h-auto w-5 shrink-0'
+          />
+          <span className='ml-3 block text-base font-medium text-gray-900'>CAD</span>
+          <span className='sr-only'>, change currency</span>
+        </a>
+      </div>
     </div>
   )
 }
@@ -32,7 +65,6 @@ export default function Example() {
   return (
     <>
       <MobileMenu open={open} setOpen={setOpen} />
-      {/* Mobile menu */}
       <header className='relative bg-white'>
         <p className='flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8'>
           Get free delivery on orders over $100
