88f8549660578c0faa87d829e98a271177273464
Alex Dickson <alexdickson53@gmail.com>
2025-10-12 13:52:56 +0200

    Updated flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   description = "Dev shell with Biome formatter";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      devShells.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           biome
         ];
       };
     };
 }
+
