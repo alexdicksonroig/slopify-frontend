1e8ee45431302c5b7bbabac601cdf7471c77c2f7
Alex Dickson <alex.dickson@immfly.com>
2025-11-09 20:25:19 +0100

    Formating

--- apps/site/app/layouts/header.tsx
+++ apps/site/app/layouts/header.tsx
@@ -12,6 +12,7 @@ export default function Example() {
     <>
       <MobileMenu open={open} setOpen={setOpen} />
       <header className="relative bg-white">
+        <title>Slopify</title>
         <p
           className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm
             font-medium text-white sm:px-6 lg:px-8"
--- apps/site/app/routes/home.tsx
+++ apps/site/app/routes/home.tsx
@@ -1,18 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
-import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
-	return [
-		{ title: "Slopify" },
-		{ name: "description", content: "Welcome to React Router!" },
-	];
-}
 
-export default function Home({ loaderData }: Route.ComponentProps) {
-	const navigate = useNavigate();
-	useEffect(() => {
-		navigate("/products");
-	}, []);
-	return <div>Hola</div>;
+export default function Home({ loaderData }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/products");
+  }, []);
+  return <div>Hola</div>;
 }
