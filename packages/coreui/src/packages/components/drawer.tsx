24bea225765170ab0e682af7392893d56a41c8a0
Alex <alexdickson53@gmail.com>
2025-12-11 13:17:30 +0100

    Added rounded borders to sidemenu

--- packages/coreui/src/packages/components/drawer.tsx
+++ packages/coreui/src/packages/components/drawer.tsx
@@ -20,25 +20,27 @@ export const Drawer: React.FC<DrawerProps> = ({
       <Overlay active={open} onClick={onClose} />
       <div
         className={clsx(
-          `z-5 fixed inset-0 h-full w-4/5 overflow-y-auto bg-white p-4 shadow-xl
-        transition duration-200 ease-in-out lg:hidden rounded-md`,
+          `z-5 fixed inset-0 h-screen w-4/5 overflow-y-auto
+        transition duration-200 ease-in-out lg:hidden p-1`,
           open ? "translate-x-0" : "-translate-x-full",
           className,
         )}
       >
-        <div className="flex px-4 pb-2 pt-5">
-          <button
-            type="button"
-            onClick={onClose}
-            className="relative -m-2 inline-flex items-center justify-center rounded-md p-2
+        <div className="bg-white p-4 shadow-xl rounded-md h-full">
+          <div className="flex px-4 pb-2 pt-5">
+            <button
+              type="button"
+              onClick={onClose}
+              className="relative -m-2 inline-flex items-center justify-center rounded-md p-2
             text-gray-400"
-          >
-            <span className="absolute -inset-0.5" />
-            <span className="sr-only">Close menu</span>
-            <X className="text-gray-400" />
-          </button>
+            >
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Close menu</span>
+              <X className="text-gray-400" />
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </>
   );
