d2de01ada531f91f07505c4b9c3070cec9bf6884
Alex <alexdickson53@gmail.com>
2026-09-14 17:27:43 +0200

    Increase gap in variants

--- app/routes/variants.tsx
+++ app/routes/variants.tsx
@@ -124,7 +124,7 @@ export default function Variants() {
   const { cards, options } = useLoaderData<typeof clientLoader>();
 
   return (
-    <div className="mx-auto w-full max-w-7xl p-3 sm:p-6 flex gap-3 flex-col">
+    <div className="mx-auto w-full max-w-7xl p-3 sm:p-6 flex gap-5 flex-col">
       <Banner />
       <div>
         <h1 className="text-[1.7rem] font-bold tracking-tight text-gray-900 md:text-3xl">
