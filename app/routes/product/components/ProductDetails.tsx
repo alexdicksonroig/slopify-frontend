16625972fdc2d15247f6c9317365093b871a314c
Alex Dickson <alexdickson53@gmail.com>
2026-09-23 19:30:22 +0200

    Fix description font size

--- app/routes/product/components/ProductDetails.tsx
+++ app/routes/product/components/ProductDetails.tsx
@@ -5,7 +5,7 @@ interface ProductDetailsProps {
 export function ProductDetails({ description }: ProductDetailsProps) {
   return (
     <div className="pt-5">
-      <p className="max-w-xl text-lg lg:text-xl leading-6 text-neutral-700">
+      <p className="max-w-xl text-md lg:text-xl leading-6 text-neutral-700">
         {description}
       </p>
     </div>
